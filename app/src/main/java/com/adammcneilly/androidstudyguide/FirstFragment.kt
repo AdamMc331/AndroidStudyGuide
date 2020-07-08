a8d2db4198514539906a362aeb5884dfc941493f
adammc331 <amcneilly331@gmail.com>
2020-07-08 19:06:58 -0400

    Adding ktlint plugin.

--- app/src/main/java/com/adammcneilly/androidstudyguide/FirstFragment.kt
+++ app/src/main/java/com/adammcneilly/androidstudyguide/FirstFragment.kt
@@ -1,12 +1,10 @@
 package com.adammcneilly.androidstudyguide
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,8 +12,9 @@ import androidx.navigation.fragment.findNavController
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
