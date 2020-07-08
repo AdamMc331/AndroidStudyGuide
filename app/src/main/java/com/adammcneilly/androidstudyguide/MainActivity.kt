8f928ffa9abc368ea87a69591583f05decea9c66
Adam McNeilly <amcneilly331@gmail.com>
2020-07-08 19:21:19 -0400

    Adding ktlint plugin. (#2)
    * Adding ktlint plugin.

--- app/src/main/java/com/adammcneilly/androidstudyguide/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/androidstudyguide/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.adammcneilly.androidstudyguide
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
