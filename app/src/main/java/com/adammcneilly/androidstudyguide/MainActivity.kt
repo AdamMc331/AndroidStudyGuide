b8dbb7743c6d3efa7fe320340fcd8738595e64b6
adammc331 <amcneilly331@gmail.com>
2020-07-08 19:39:36 -0400

    Adding Detekt plugin.

--- app/src/main/java/com/adammcneilly/androidstudyguide/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/androidstudyguide/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    // Test change
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
