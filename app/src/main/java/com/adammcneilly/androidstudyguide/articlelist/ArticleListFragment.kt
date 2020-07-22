e226ca0824e51277ec24385ddf95828c9b592817
adammc331 <amcneilly331@gmail.com>
2020-07-22 19:01:41 -0400

    Adding ArticleListViewModel.kt

--- app/src/main/java/com/adammcneilly/androidstudyguide/articlelist/ArticleListFragment.kt
+++ app/src/main/java/com/adammcneilly/androidstudyguide/articlelist/ArticleListFragment.kt
@@ -7,6 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.adammcneilly.androidstudyguide.data.ArticleRepository
 import com.adammcneilly.androidstudyguide.data.InMemoryArticleService
@@ -17,8 +20,24 @@ class ArticleListFragment : Fragment(), ArticleClickListener {
 
     private lateinit var binding: FragmentArticleListBinding
     private lateinit var adapter: ArticleAdapter
+    private lateinit var viewModel: ArticleListViewModel
 
-    private val articleRepository: ArticleRepository = InMemoryArticleService()
+    private val articleListViewModelFactory = object : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            val repository: ArticleRepository = InMemoryArticleService()
+
+            @Suppress("UNCHECKED_CAST")
+            return ArticleListViewModel(
+                articleRepository = repository
+            ) as T
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this, articleListViewModelFactory).get(ArticleListViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,7 +55,13 @@ class ArticleListFragment : Fragment(), ArticleClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        adapter.articles = articleRepository.fetchArticles()
+        subscribeToViewModel()
+    }
+
+    private fun subscribeToViewModel() {
+        viewModel.articles.observe(viewLifecycleOwner, Observer { articles ->
+            adapter.articles = articles
+        })
     }
 
     private fun setupRecyclerView() {
