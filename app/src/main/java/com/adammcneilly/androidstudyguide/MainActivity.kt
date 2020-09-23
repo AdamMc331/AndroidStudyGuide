d8084dd80b7da41e89f7618075d260bd0e1637e5
adammc331 <amcneilly331@gmail.com>
2020-09-23 19:17:49 -0400

    Connecting BottomNavigationView with NavController.

--- app/src/main/java/com/adammcneilly/androidstudyguide/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/androidstudyguide/MainActivity.kt
@@ -3,6 +3,7 @@ package com.adammcneilly.androidstudyguide
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,20 +25,6 @@ class MainActivity : AppCompatActivity() {
     private fun setupBottomNavigationMenu() {
         val bottomNavigationMenu: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
-        bottomNavigationMenu.setOnNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.all_articles -> {
-                    findNavController(R.id.nav_host_fragment).navigate(R.id.ArticleListFragment)
-                    true
-                }
-                R.id.bookmarks -> {
-                    findNavController(R.id.nav_host_fragment).navigate(R.id.BookmarkListFragment)
-                    true
-                }
-                else -> {
-                    false
-                }
-            }
-        }
+        bottomNavigationMenu.setupWithNavController(findNavController(R.id.nav_host_fragment))
     }
 }
