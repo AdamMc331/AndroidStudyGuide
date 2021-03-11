8e943abcd52896c3852d4404108138105d7f1146
adammc331 <amcneilly331@gmail.com>
2021-03-10 20:04:08 -0500

    Handling click listeners.

--- app/src/androidTest/java/com/adammcneilly/androidstudyguide/compose/ArticleListItemTest.kt
+++ app/src/androidTest/java/com/adammcneilly/androidstudyguide/compose/ArticleListItemTest.kt
@@ -4,8 +4,11 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
 import com.adammcneilly.androidstudyguide.models.Article
 import com.adammcneilly.androidstudyguide.util.HtmlString
+import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
 import org.junit.Test
 
@@ -34,7 +37,8 @@ class ArticleListItemTest {
         }
 
         composeTestRule.onNodeWithText(title, useUnmergedTree = true).assertIsDisplayed()
-        composeTestRule.onNodeWithText("By Adam McNeilly", useUnmergedTree = true).assertIsDisplayed()
+        composeTestRule.onNodeWithText("By Adam McNeilly", useUnmergedTree = true)
+            .assertIsDisplayed()
         composeTestRule.onNodeWithContentDescription("Bookmarked").assertIsDisplayed()
         composeTestRule.onNodeWithContentDescription("Article Tags").assertDoesNotExist()
     }
@@ -59,11 +63,45 @@ class ArticleListItemTest {
         }
 
         composeTestRule.onNodeWithText(title, useUnmergedTree = true).assertIsDisplayed()
-        composeTestRule.onNodeWithText("By Adam McNeilly", useUnmergedTree = true).assertIsDisplayed()
+        composeTestRule.onNodeWithText("By Adam McNeilly", useUnmergedTree = true)
+            .assertIsDisplayed()
         composeTestRule.onNodeWithContentDescription("Not Bookmarked").assertIsDisplayed()
         composeTestRule.onNodeWithContentDescription("Article Tags").assertIsDisplayed()
 
         composeTestRule.onNodeWithText("Jetpack", useUnmergedTree = true).assertIsDisplayed()
         composeTestRule.onNodeWithText("Compose", useUnmergedTree = true).assertIsDisplayed()
+    }
+
+    @Test
+    fun handleClickListeners() {
+        val title = "My Title"
+
+        val article = Article(
+            htmlTitle = HtmlString(title),
+            authorName = "Adam McNeilly",
+            bookmarked = true,
+            tags = emptyList(),
+        )
+
+        var bookmarkClicked = false
+        var articleClicked = false
+
+        composeTestRule.setContent {
+            ArticleListItem(
+                article = article,
+                onBookmarkClicked = {
+                    bookmarkClicked = true
+                },
+                onArticleClicked = {
+                    articleClicked = true
+                }
+            )
+        }
+
+        composeTestRule.onNodeWithContentDescription("Bookmarked").performClick()
+        assertThat(bookmarkClicked).isTrue()
+
+        composeTestRule.onRoot().performClick()
+        assertThat(articleClicked).isTrue()
     }
 }
