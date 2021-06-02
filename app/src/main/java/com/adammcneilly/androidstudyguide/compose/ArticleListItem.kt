46128e03a1dd96550238b4f1cf10e1222bfa904e
amcneilly <amcneilly331@gmail.com>
2021-06-02 14:59:42 -0400

    Formatting fix.

--- app/src/main/java/com/adammcneilly/androidstudyguide/compose/ArticleListItem.kt
+++ app/src/main/java/com/adammcneilly/androidstudyguide/compose/ArticleListItem.kt
@@ -2,7 +2,6 @@ package com.adammcneilly.androidstudyguide.compose
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
