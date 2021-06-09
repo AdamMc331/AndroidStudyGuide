2be0aa060782825a8b39c2157ef2b78f00b786ee
Adam McNeilly <amcneilly331@gmail.com>
2021-06-09 19:41:49 -0400

    Fixing bookmark UI update bug and testing. (#65)

--- app/src/androidTest/java/com/adammcneilly/androidstudyguide/compose/ArticleListTest.kt
+++ app/src/androidTest/java/com/adammcneilly/androidstudyguide/compose/ArticleListTest.kt
@@ -1,8 +1,14 @@
 package com.adammcneilly.androidstudyguide.compose
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.printToLog
 import com.adammcneilly.androidstudyguide.models.Article
 import com.adammcneilly.androidstudyguide.util.HtmlString
@@ -40,5 +46,45 @@ class ArticleListTest {
 //        composeTestRule.onNodeWithText("Article Number: 7", useUnmergedTree = true).assertExists()
 //        composeTestRule.onNodeWithText("Article Number: 8", useUnmergedTree = true).assertExists()
 //        composeTestRule.onNodeWithText("Article Number: 9", useUnmergedTree = true).assertExists()
+    }
+
+    @Test
+    fun clickingBookmarkIconUpdatesUI() {
+        // Start off with a test article that is not
+        // bookmarked by the user.
+        val testArticle = Article(
+            htmlTitle = HtmlString("Test Article"),
+            bookmarked = false,
+        )
+
+        composeTestRule.setContent {
+            // Maintain a local article list, that can be updated
+            // on click, so the UI should re-render.
+            var articleList: List<Article> by remember {
+                mutableStateOf(listOf(testArticle))
+            }
+
+            ArticleList(
+                articles = articleList,
+                // When a bookmark icon is clicked, just map the entire list
+                // to say the items are bookmarked.
+                // Since this test only has one article, this is fine.
+                onBookmarkClicked = {
+                    articleList = articleList.map {
+                        it.copy(bookmarked = true)
+                    }
+                },
+                onArticleClicked = { /*TODO*/ },
+            )
+        }
+
+        // First, verify that a icon for a not-bookmarked article exists.
+        composeTestRule.onNodeWithContentDescription("Not Bookmarked").assertExists()
+
+        // Click on the not-bookmarked article.
+        composeTestRule.onNodeWithContentDescription("Not Bookmarked").performClick()
+
+        // Verify that an icon for a bookmarked article exists.
+        composeTestRule.onNodeWithContentDescription("Bookmarked").assertExists()
     }
 }
