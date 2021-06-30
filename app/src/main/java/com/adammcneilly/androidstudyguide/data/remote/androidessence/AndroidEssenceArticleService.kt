a65c09d0e0b2905ed9695fe8025a86928990c2ab
amcneilly <amcneilly331@gmail.com>
2021-06-30 15:42:28 -0400

    Removing jcenter, updating detekt, fixing detekt issue.

--- app/src/main/java/com/adammcneilly/androidstudyguide/data/remote/androidessence/AndroidEssenceArticleService.kt
+++ app/src/main/java/com/adammcneilly/androidstudyguide/data/remote/androidessence/AndroidEssenceArticleService.kt
@@ -54,7 +54,7 @@ class AndroidEssenceArticleService @Inject constructor(
 }
 
 /**
- * TODO: If any of these networking values are null, we should throw an error so we're aware.
+ * If any of these networking values are null, we should throw an error so we're aware.
  *  but we've decided not to crash, so we don't ruin the UX.
  *  https://github.com/AdamMc331/AndroidStudyGuide/issues/28
  */
